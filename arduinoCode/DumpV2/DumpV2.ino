011fa74bda948297d4e81bd81deaf4bfb0a6156c
Sorachai <tdeathnotet@hotmail.com>
2019-09-04 13:17:43 +0700

    New SQL & All Button TV_remote

--- arduinoCode/DumpV2/DumpV2.ino
+++ arduinoCode/DumpV2/DumpV2.ino
@@ -11,8 +11,8 @@
 const char* MY_SSID = "icute3";
 const char* MY_PWD =  "thinkbeyond03";
 
-String tv_On = "",tv_volUp = "",tv_volDown = "";
-int buffOn,buffVolUp,buffVolDown; //buffer botton decoder
+//String tv_On = "",tv_volUp = "",tv_volDown = "";
+//int buffOn,buffVolUp,buffVolDown; //buffer botton decoder
 
 //const char* MY_SSID = "26SW_WIFI_2.4G";
 //const char* MY_PWD =  "58543206";
@@ -28,10 +28,6 @@ IRsend irsend(kIrLed);  // Set the GPIO to be used to sending the message.
 
 
 //void convertStrtoArr();
-int toIntArray();
-String httpJSON();
-
-
 #if DECODE_AC
 const uint8_t kTimeout = 50;
 #else   // DECODE_AC
@@ -43,119 +39,6 @@ const uint16_t kMinUnknownSize = 12;
 
 IRrecv irrecv(kRecvPin, kCaptureBufferSize, kTimeout, true);
 decode_results results;  // Somewhere to store the results
-
-
-int count = 0;
-void setup() {
-
-  Serial.begin(115200);
-  Serial.printf("\nIRrecvDumpV2 is now running and waiting for IR input on Pin "
-                  "%d\n", kRecvPin);
-  #if DECODE_HASH
-    // Ignore messages with less than minimum on or off pulses.
-    irrecv.setUnknownThreshold(kMinUnknownSize);
-  #endif                  // DECODE_HASH
-    irrecv.enableIRIn();  // Start the receiver
-    
-  Serial.print("Connecting to "+*MY_SSID);
-  WiFi.begin(MY_SSID, MY_PWD);
-  Serial.println("going into wl connect");
-
-  while (WiFi.status() != WL_CONNECTED){
-      delay(500);
-      Serial.print(".");
-  }
-  Serial.println("wl connected");
-  Serial.println("");
-  Serial.println("WIFI connected\n ");
-  Serial.println("");
-//  
-
-
-
-    Serial.println("Select the remote & decoder : ");
-    Serial.println("1. TV remote");
-    Serial.println("2. AC remote");
-//    irsend.begin();
-}
-
-
-
-void loop() {
-    if(irrecv.decode(&results)) {
-      if(Serial.available()){
-        char input = Serial.read();
- /*----------------------------------Decode Tv remote Button ON-----------------------------------------*/       
-         if( input == '1'){  
-            Serial.println("You Select TV remote & Decoder Button ON ");  
-            buffOn = uint64ToString(getCorrectedRawLength(&results), 10).toInt();
-            tv_On = resultToSourceCode(&results);// Feed the WDT as the text output can take a while to print.
-            count += 1;
-//            yield(); 
-            // uint16_t command[bufferSize];
-            Serial.print("tv_On[");Serial.print(buffOn);Serial.print("] :");Serial.println(tv_On);
-            yield(); 
-            Serial.println("Decoder Success!!"); Serial.println("");  
-            httpJSON("tv_On",tv_On);
-          }
- /*----------------------------------Decode Tv remote Button VOL + -----------------------------------------*/             
-          else if( input == '2'){   
-            Serial.println("You Select TV remote & Decoder Button Vol +");
-            buffVolUp = uint64ToString(getCorrectedRawLength(&results), 10).toInt();
-            tv_volUp = resultToSourceCode(&results);// Feed the WDT as the text output can take a while to print.
-            count += 1;
-//            yield(); 
-            // uint16_t command[bufferSize];
-            Serial.print("tv_volUp[");Serial.print(buffVolUp);Serial.print("] :");Serial.println(tv_volUp);
-            yield(); 
-            Serial.println("Decoder Success!!"); Serial.println("");
-            httpJSON("tv_volUp",tv_volUp);      
-          } 
-  /*----------------------------------Decode Tv remote Button VOL - -----------------------------------------*/     
-           else if( input == '3'){   
-            Serial.println("You Select TV remote & Decoder Button Vol -");
-            buffVolDown = uint64ToString(getCorrectedRawLength(&results), 10).toInt();
-            tv_volDown = resultToSourceCode(&results);// Feed the WDT as the text output can take a while to print.
-            count += 1;
-            // uint16_t command[bufferSize];
-            Serial.print("tv_volDown[");Serial.print(buffVolDown);Serial.print("] :");Serial.println(tv_volDown);
-            yield(); 
-            Serial.println("Decoder Success!!"); Serial.println("");
-            httpJSON("tv_volDown",tv_volDown);     
-          }   
-          else{
-            Serial.println("Enter Button Again");
-          }
-    }
-
-} 
-
-//    if(Serial.available()){
-//        input = Serial.read();
-//        if(input == '1')
-//          Serial.println("You Select TV remote & Enter your Enncode");    
-//    }
-
-//   if (irrecv.decode(&results)) {
-//    //rawLengthData----------------------------------------------------------------
-//     Serial.println("");
-//     int bufferSize = uint64ToString(getCorrectedRawLength(&results), 10).toInt();
-//     uint16_t command[bufferSize];
-
-//  //rawData------------------------------------------------------------------
-//      //Serial.print("&results :"); 
-//      String rawData = resultToSourceCode(&results);
-
-//      Serial.print("rawData[");Serial.print(bufferSize);Serial.print("] :");
-//      Serial.println(rawData);
-//      Serial.print("sendJSON[");Serial.print(bufferSize);Serial.print("] :");
-//      httpJSON(rawData);  //ส่งJSON ไปยัง http
-//      toIntArray(rawData,bufferSize,command);  //ฟังก์ชั้น Char* to Int Array  
-
-//      //irsend.sendRaw(command,bufferSize, 38);  // Send a raw data capture at 38kHz.
-//   }
-    
-}
 
 int toIntArray(String str,int buffSize ,uint16_t* command){
     int str_len = str.length()+1;
@@ -192,7 +75,7 @@ String httpJSON(String Data,String rawData){
       JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
       Serial.println(JSONmessageBuffer);
       HTTPClient http;
-      http.begin("http://192.168.1.13:4000/control/api/jsonn");      //ปลายทางที่เราจะส่ง JSONไป
+      http.begin("http://192.168.1.18:4000/control/api/jsonn");      //ปลายทางที่เราจะส่ง JSONไป
       http.addHeader("Content-Type", "application/json");  //Specify content-type header
 
       int httpCode = http.POST(JSONmessageBuffer);   //Send the request
@@ -206,4 +89,239 @@ String httpJSON(String Data,String rawData){
     }else {
       Serial.println("Error in WiFi connection"); 
     }
+
+
+    
+}
+
+
+void setup() {
+
+  Serial.begin(115200);
+  Serial.printf("\nIRrecvDumpV2 is now running and waiting for IR input on Pin "
+                  "%d\n", kRecvPin);
+  #if DECODE_HASH
+    // Ignore messages with less than minimum on or off pulses.
+    irrecv.setUnknownThreshold(kMinUnknownSize);
+  #endif                  // DECODE_HASH
+    irrecv.enableIRIn();  // Start the receiver
+    
+  Serial.print("Connecting to "+*MY_SSID);
+  WiFi.begin(MY_SSID, MY_PWD);
+  Serial.println("going into wl connect");
+
+  while (WiFi.status() != WL_CONNECTED){
+      delay(500);
+      Serial.print(".");
+  }
+  Serial.println("wl connected");
+  Serial.println("");
+  Serial.println("WIFI connected\n ");
+  Serial.println("");
+//  
+
+
+
+    Serial.println("--------- DECOD TV REMOTE-------------");
+    
+    Serial.println("Enter ' 0 ' ON Button ");
+    Serial.println("Enter ' 5 ' OK Button");
+    Serial.println("Enter ' 8 ' ▲ Button");
+    Serial.println("Enter ' 4 ' ◀ Button");
+    Serial.println("Enter ' 6 ' ▶ Button");
+    Serial.println("Enter ' 2 ' ▼ Button");
+    Serial.println("Enter ' + ' Vol+ Button ");
+    Serial.println("Enter ' - ' vol- Button ");
+    Serial.println("Enter ' 9 ' CH ▲ Button");
+    Serial.println("Enter ' 3 ' CH ▼ Button");
+    Serial.println("Enter ' 1 ' ↺Return,Exit Button");
+    Serial.println("Enter ' 7 ' Mute Button");
+    
+    
+
+    
+    
+//    irsend.begin();
+}
+
+
+
+void loop() {
+    if(irrecv.decode(&results)) {
+      if(Serial.available()){
+        char input = Serial.read();
+//___________________________________________________TV REMOTE__________________________________________________
+        
+ /*----------------------------------Decode Tv remote  ' 0 ' ON Button-----------------------------------------*/       
+         if( input == '0'){  
+            Serial.println("You Select TV remote & Decoder ON Button ");  
+            int buffOn = uint64ToString(getCorrectedRawLength(&results), 10).toInt();
+            String tv_On = resultToSourceCode(&results);// Feed the WDT as the text output can take a while to print.
+           yield(); 
+            // uint16_t command[bufferSize];
+            Serial.print("tv_On[");Serial.print(buffOn);Serial.print("] :");Serial.println(tv_On);
+            // yield(); 
+            Serial.println("Decoder Success!!"); Serial.println("");  
+            httpJSON("tv_On",tv_On);
+          }
+ /*----------------------------------Decode Tv remote  ' + ' Vol+ Button -----------------------------------------*/             
+          else if( input == '+'){   
+            Serial.println("You Select TV remote & Decoder Vol+ Button");
+            int buffVolUp = uint64ToString(getCorrectedRawLength(&results), 10).toInt();
+            String tv_volUp = resultToSourceCode(&results);// Feed the WDT as the text output can take a while to print.
+           yield(); 
+            // uint16_t command[bufferSize];
+            Serial.print("tv_volUp[");Serial.print(buffVolUp);Serial.print("] :");Serial.println(tv_volUp);
+            // yield(); 
+            Serial.println("Decoder Success!!"); Serial.println("");
+            httpJSON("tv_volUp",tv_volUp);      
+          } 
+  /*----------------------------------Decode Tv remote  ' - ' vol- Button-----------------------------------------*/     
+           else if( input == '-'){   
+            Serial.println("You Select TV remote & Decoder vol- Button");
+            int buffVolDown = uint64ToString(getCorrectedRawLength(&results), 10).toInt();
+            String tv_volDown = resultToSourceCode(&results);// Feed the WDT as the text output can take a while to print.
+            // uint16_t command[bufferSize];
+            Serial.print("tv_volDown[");Serial.print(buffVolDown);Serial.print("] :");Serial.println(tv_volDown);
+            // yield(); 
+            Serial.println("Decoder Success!!"); Serial.println("");
+            httpJSON("tv_volDown",tv_volDown);     
+          }  
+/*----------------------------------Decode Tv remote  ' 5 ' OK Button -----------------------------------------*/ 
+           else if( input == '5'){   
+            Serial.println("You Select TV remote & Decoder OK Button");
+            int buffOK = uint64ToString(getCorrectedRawLength(&results), 10).toInt();
+            String tv_OK = resultToSourceCode(&results);// Feed the WDT as the text output can take a while to print.
+            // uint16_t command[bufferSize];
+            Serial.print("tv_OK[");Serial.print(buffOK);Serial.print("] :");Serial.println(tv_OK);
+            // yield(); 
+            Serial.println("Decoder Success!!"); Serial.println("");
+            httpJSON("tv_OK",tv_OK);     
+          }  
+/*----------------------------------Decode Tv remote  ' 8 ' ▲ Button-----------------------------------------*/
+           else if( input == '8'){   
+            Serial.println("You Select TV remote & Decoder ▲ Button");
+            int buffUp = uint64ToString(getCorrectedRawLength(&results), 10).toInt();
+            String tv_Up = resultToSourceCode(&results);// Feed the WDT as the text output can take a while to print.
+            // uint16_t command[bufferSize];
+            Serial.print("tv_Up[");Serial.print(buffUp);Serial.print("] :");Serial.println(tv_Up);
+            // yield(); 
+            Serial.println("Decoder Success!!"); Serial.println("");
+            httpJSON("tv_Up",tv_Up);     
+          }    
+/*----------------------------------Decode Tv remote  ' 4 ' ◀ Button-----------------------------------------*/
+           else if( input == '4'){   
+            Serial.println("You Select TV remote & Decoder ◀ Button");
+            int buffLeft = uint64ToString(getCorrectedRawLength(&results), 10).toInt();
+            String tv_Left = resultToSourceCode(&results);// Feed the WDT as the text output can take a while to print.
+            // uint16_t command[bufferSize];
+            Serial.print("tv_Left[");Serial.print(buffLeft);Serial.print("] :");Serial.println(tv_Left);
+            // yield(); 
+            Serial.println("Decoder Success!!"); Serial.println("");
+            httpJSON("tv_Left",tv_Left);     
+          } 
+/*----------------------------------Decode Tv remote  ' 6 ' ▶ Button-----------------------------------------*/ 
+           else if( input == '6'){   
+            Serial.println("You Select TV remote & Decoder ▶ Button");
+            int buffRight = uint64ToString(getCorrectedRawLength(&results), 10).toInt();
+            String tv_Right = resultToSourceCode(&results);// Feed the WDT as the text output can take a while to print.
+            // uint16_t command[bufferSize];
+            Serial.print("tv_Right[");Serial.print(buffRight);Serial.print("] :");Serial.println(tv_Right);
+            // yield(); 
+            Serial.println("Decoder Success!!"); Serial.println("");
+            httpJSON("tv_Right",tv_Right);     
+          }
+/*----------------------------------Decode Tv remote  ' 2 ' ▼ Button-----------------------------------------*/ 
+           else if( input == '2'){   
+            Serial.println("You Select TV remote & Decoder ▼ Button");
+            int buffDown = uint64ToString(getCorrectedRawLength(&results), 10).toInt();
+            String tv_Down = resultToSourceCode(&results);// Feed the WDT as the text output can take a while to print.
+            // uint16_t command[bufferSize];
+            Serial.print("tv_Down[");Serial.print(buffDown);Serial.print("] :");Serial.println(tv_Down);
+            // yield(); 
+            Serial.println("Decoder Success!!"); Serial.println("");
+            httpJSON("tv_Down",tv_Down);     
+          }
+/*----------------------------------Decode Tv remote  ' 9 ' CH ▲ Button-----------------------------------------*/
+           else if( input == '9'){   
+            Serial.println("You Select TV remote & Decoder CH ▲ Button");
+            int buffCHUp = uint64ToString(getCorrectedRawLength(&results), 10).toInt();
+            String tv_CHUp = resultToSourceCode(&results);// Feed the WDT as the text output can take a while to print.
+            // uint16_t command[bufferSize];
+            Serial.print("tv_CHUp[");Serial.print(buffCHUp);Serial.print("] :");Serial.println(tv_CHUp);
+            // yield(); 
+            Serial.println("Decoder Success!!"); Serial.println("");
+            httpJSON("tv_CHUp",tv_CHUp);     
+          }
+/*----------------------------------Decode Tv remote  ' 3 ' CH ▼ Button-----------------------------------------*/ 
+           else if( input == '3'){   
+            Serial.println("You Select TV remote & Decoder CH ▼ Button");
+            int buffCHDown = uint64ToString(getCorrectedRawLength(&results), 10).toInt();
+            String tv_CHDown = resultToSourceCode(&results);// Feed the WDT as the text output can take a while to print.
+            // uint16_t command[bufferSize];
+            Serial.print("tv_CHDown[");Serial.print(buffCHDown);Serial.print("] :");Serial.println(tv_CHDown);
+            // yield(); 
+            Serial.println("Decoder Success!!"); Serial.println("");
+            httpJSON("tv_CHDown",tv_CHDown);     
+          }
+/*----------------------------------Decode Tv remote  ' 1 ' ↺Return,Exit Button-----------------------------------------*/ 
+           else if( input == '1'){   
+            Serial.println("You Select TV remote & Decoder ↺Return,Exit Button");
+            int buffReturn = uint64ToString(getCorrectedRawLength(&results), 10).toInt();
+            String tv_Return = resultToSourceCode(&results);// Feed the WDT as the text output can take a while to print.
+            // uint16_t command[bufferSize];
+            Serial.print("tv_Return[");Serial.print(buffReturn);Serial.print("] :");Serial.println(tv_Return);
+            // yield(); 
+            Serial.println("Decoder Success!!"); Serial.println("");
+            httpJSON("tv_Return",tv_Return);     
+          }
+/*----------------------------------Decode Tv remote  ' 7 ' Mute Button-----------------------------------------*/  
+            else if( input == '7'){   
+            Serial.println("You Select TV remote & Decoder Mute Button");
+            int buffMute = uint64ToString(getCorrectedRawLength(&results), 10).toInt();
+            String tv_Mute = resultToSourceCode(&results);// Feed the WDT as the text output can take a while to print.
+            // uint16_t command[bufferSize];
+            Serial.print("tv_Mute[");Serial.print(buffMute);Serial.print("] :");Serial.println(tv_Mute);
+            // yield(); 
+            Serial.println("Decoder Success!!"); Serial.println("");
+            httpJSON("tv_Mute",tv_Mute);     
+          }
+
+          else{
+            Serial.println("Enter Button Again.... ");
+          }
+    }
+
+  } 
+
+ /*
+             uint16_t rawData[233] = {590, 17808,  3026, 8946,  482, 508,  558, 1460,  470, 498,  512, 482,  520, 474,  520, 474,  522, 472,  520, 474,  522, 474,  522, 1494,  494, 476,  520, 478,  516, 1494,  494, 498,  496, 500,  492, 1522,  496, 1494,  472, 1518,  518, 1472,  496, 1494,  494, 474,  520, 474,  520, 474,  522, 474,  520, 474,  520, 476,  518, 500,  496, 478,  516, 498,  498, 498,  494, 502,  514, 482,  552, 440,  522, 472,  490, 506,  516, 478,  522, 472,  522, 474,  520, 472,  522, 474,  520, 472,  522, 474,  520, 474,  520, 476,  518, 498,  496, 478,  516, 498,  498, 500,  494, 500,  516, 476,  556, 442,  522, 474,  490, 1526,  494, 1496,  496, 1494,  496, 1494,  494, 2976,  2996, 8948,  518, 1496,  496, 478,  516, 498,  496, 464,  532, 500,  486, 510,  544, 450,  552, 442,  494, 500,  490, 1524,  496, 474,  522, 472,  522, 1494,  494, 474,  522, 1494,  494, 1494,  496, 478,  516, 1494,  496, 1520,  468, 1522,  526, 442,  496, 1518,  490, 478,  520, 1496,  496, 1494,  494, 474,  520, 474,  522, 472,  544, 1470,  496, 1494,  496, 1494,  522, 474,  496, 500,  512, 482,  552, 442,  522, 470,  492, 1524,  496, 474,  544, 448,  546, 1468,  496, 1494,  494, 1494,  520, 452,  542, 1470,  520, 1470,  498, 496,  496, 502,  542, 452,  550, 444,  496, 498,  494, 502,  490, 504,  544, 1472,  492, 1496,  494, 1494,  496, 1494,  496};  // SAMSUNG_AC
+             uint8_t state[14] = {0x02, 0x92, 0x0F, 0x00, 0x00, 0x00, 0xF0, 0x01, 0xD2, 0xAE, 0x71, 0x90, 0x1B, 0xF0};
+
+             */
+//    if(Serial.available()){
+//        input = Serial.read();
+//        if(input == '1')
+//          Serial.println("You Select TV remote & Enter your Enncode");    
+//    }
+
+//   if (irrecv.decode(&results)) {
+//    //rawLengthData----------------------------------------------------------------
+//     Serial.println("");
+//     int bufferSize = uint64ToString(getCorrectedRawLength(&results), 10).toInt();
+//     uint16_t command[bufferSize];
+
+//  //rawData------------------------------------------------------------------
+//      //Serial.print("&results :"); 
+//      String rawData = resultToSourceCode(&results);
+
+//      Serial.print("rawData[");Serial.print(bufferSize);Serial.print("] :");
+//      Serial.println(rawData);
+//      Serial.print("sendJSON[");Serial.print(bufferSize);Serial.print("] :");
+//      httpJSON(rawData);  //ส่งJSON ไปยัง http
+//      toIntArray(rawData,bufferSize,command);  //ฟังก์ชั้น Char* to Int Array  
+
+//      //irsend.sendRaw(command,bufferSize, 38);  // Send a raw data capture at 38kHz.
+//   }
+    
 }
--- smart_home.sql
+++ smart_home.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 15, 2019 at 10:43 AM
+-- Generation Time: Sep 04, 2019 at 05:55 AM
 -- Server version: 8.0.17
 -- PHP Version: 7.3.8
 
@@ -25,15 +25,39 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `air_remote`
+--
+
+CREATE TABLE `air_remote` (
+  `air_id` int(11) NOT NULL,
+  `device_id` int(11) NOT NULL,
+  `air_speedDown` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `air_power` text NOT NULL,
+  `air_tempUp` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `air_tempDown` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `air_speedUp` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `air_timer` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `air_remote`
+--
+
+INSERT INTO `air_remote` (`air_id`, `device_id`, `air_speedDown`, `air_power`, `air_tempUp`, `air_tempDown`, `air_speedUp`, `air_timer`) VALUES
+(0, 0, '', '554, 17962,  2990, 8954,  514, 478,  516, 1496,  492, 504,  492, 502,  490, 502,  466, 528,  464, 530,  464, 530,  490, 504,  492, 1524,  494, 476,  518, 476,  518, 1498,  492, 476,  520, 476,  516, 1496,  494, 1496,  494, 1524,  466, 1522,  440, 1550,  438, 530,  488, 506,  490, 506,  516, 478,  518, 476,  520, 476,  520, 474,  520, 476,  518, 476,  520, 476,  518, 478,  518, 478,  516, 480,  514, 502,  492, 502,  492, 502,  466, 528,  464, 530,  466, 528,  488, 508,  490, 504,  516, 478,  518, 476,  518, 476,  518, 476,  520, 476,  518, 476,  518, 476,  518, 476,  516, 480,  516, 480,  514, 502,  492, 1524,  440, 1550,  440, 1550,  462, 1526,  494, 2976,  2996, 8950,  520, 1496,  494, 476,  518, 476,  520, 476,  518, 478,  516, 478,  516, 480,  514, 502,  492, 502,  492, 1522,  442, 528,  464, 530,  464, 1552,  462, 506,  492, 502,  518, 1498,  492, 476,  518, 1498,  492, 1496,  494, 1496,  494, 1496,  494, 1522,  466, 1524,  442, 1548,  438, 1550,  464, 504,  516, 478,  518, 476,  520, 1496,  492, 1496,  494, 1496,  492, 478,  516, 480,  516, 480,  514, 502,  492, 502,  492, 1524,  440, 1550,  438, 530,  488, 1528,  490, 1500,  492, 1496,  494, 1496,  492, 1496,  494, 1496,  494, 478,  516, 480,  514, 502,  492, 502,  492, 502,  466, 528,  466, 530,  464, 1550,  464, 1526,  492, 1498,  492, 1496,  494', '', '', '', '');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `devices`
 --
 
 CREATE TABLE `devices` (
-  `device_id` int(11) NOT NULL,
-  `device_name` text NOT NULL,
-  `device_room` text NOT NULL,
-  `wifi_user` varchar(50) NOT NULL,
-  `wifi_pass` varchar(50) NOT NULL,
+  `device_id` int(10) NOT NULL,
+  `device_name` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `device_room` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `wifi_user` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `wifi_pass` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `device_enable` bit(1) NOT NULL DEFAULT b'0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -43,28 +67,41 @@ CREATE TABLE `devices` (
 
 INSERT INTO `devices` (`device_id`, `device_name`, `device_room`, `wifi_user`, `wifi_pass`, `device_enable`) VALUES
 (1, 'Sorachai', 'Living Room', '', '', b'0'),
-(2, 'Ponglada', 'BedRoom', '', '', b'0');
+(2, 'Ponglada', 'BedRoom', '', '', b'0'),
+(3, 'Suchada', 'Chicken', 'icute2', 'think2018', b'1');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `remote_air`
+-- Table structure for table `tv_remote`
 --
 
-CREATE TABLE `remote_air` (
+CREATE TABLE `tv_remote` (
+  `tv_id` int(11) NOT NULL,
   `device_id` int(11) NOT NULL,
-  `air_enable` bit(1) NOT NULL DEFAULT b'0',
-  `air_speedDown` text NOT NULL,
-  `air_power` text NOT NULL,
-  `air_tempUp` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `air_tempDown` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `air_speedUp` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `air_timer` text NOT NULL
+  `tv_On` text NOT NULL,
+  `tv_volUp` text NOT NULL,
+  `tv_volDown` text NOT NULL,
+  `tv_OK` text NOT NULL,
+  `tv_Up` text NOT NULL,
+  `tv_Left` text NOT NULL,
+  `tv_Right` text NOT NULL,
+  `tv_Down` text NOT NULL,
+  `tv_CHUp` text NOT NULL,
+  `tv_CHDown` text NOT NULL,
+  `tv_Return` text NOT NULL,
+  `tv_Mute` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `air_remote`
+--
+ALTER TABLE `air_remote`
+  ADD PRIMARY KEY (`air_id`);
 
 --
 -- Indexes for table `devices`
@@ -80,7 +117,7 @@ ALTER TABLE `devices`
 -- AUTO_INCREMENT for table `devices`
 --
 ALTER TABLE `devices`
-  MODIFY `device_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `device_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
